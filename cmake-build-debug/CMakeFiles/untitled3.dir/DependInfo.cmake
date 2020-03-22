73cc5cdd3964ce7a74a302fa9b4277d18fcdd4ce
acidhole <johny.sp@mail.ru>
2020-03-23 01:42:40 +0300

    final

--- cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/adminlocal/GIT/main.cpp" "/home/adminlocal/GIT/cmake-build-debug/CMakeFiles/untitled3.dir/main.cpp.o"
+  "/home/gridlock/GIT/Study/main.cpp" "/home/gridlock/GIT/Study/cmake-build-debug/CMakeFiles/untitled3.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
